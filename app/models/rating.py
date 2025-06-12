1373b27f8a52c7fd09fee24f2e5400c561c0d6e1
liamnaule <liamnaule@gmail.com>
2025-06-13 01:04:16 +0300

    Added Rating Model

---
+++ app/models/rating.py
@@ -0,0 +1,13 @@
+from app import db
+from datetime import datetime
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('sessions.session_id'), nullable=False)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Rating {self.rating_id}>'
