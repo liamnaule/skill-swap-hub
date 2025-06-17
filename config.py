01cfd18c5c53a939a7ec423fd56e159e34ca6a81
liamnaule <liamnaule@gmail.com>
2025-06-17 11:07:57 +0300

    fixed migrations

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///skill_hub.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .rating import Rating
+from .skill import Skill
+from .session import Session
+from .user import User
